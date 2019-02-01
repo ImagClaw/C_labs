20ae9c315550aeff5da739b6fb5be11a216f6af9
ImagClaw <dal.whelpley@gmail.com>
2019-02-01 10:00:55 -0600

    added lab4a

--- ch04/LABS/Lab4A.c
+++ ch04/LABS/Lab4A.c
@@ -1,7 +1,8 @@
-/* Student Age Arrays and Quote Array
+/* Input a single char and then print the next char in the sequence
+# Use getchar() and putchar()
 *
 *  name: Whelpley
-*  date: 30 Jan 2019
+*  date: 01 Feb 2019
 *  project: Demo 3A
 *
 */
@@ -9,5 +10,13 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    int myInput = 0;
+    printf("Enter a single char: ");
+    myInput = getchar();
+    printf("The next character in the sequence is: ");
+    putchar(myInput + 1);
+
+    // Calling getchar() which is assigned to myInput
+    getchar();
+    return 0;
 }
