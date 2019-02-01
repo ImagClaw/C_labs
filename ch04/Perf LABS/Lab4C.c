e38a127fcd774c674e79374a3aaacde3bf48ff9f
ImagClaw <dal.whelpley@gmail.com>
2019-02-01 13:40:10 -0600

    Lab4C added

---
+++ ch04/Perf LABS/Lab4C.c
@@ -0,0 +1,31 @@
+/* Read a string from stdin into a char array of dimension [10]
+*  Write that string to stdout
+*  Use fgets() and fputs()
+*  Test it with the following input:
+*       - Program
+*       - Class rules
+*       - <Enter>
+*       - 1<tab>2<space>3456789
+*       - |$(5)$|
+*       - ~\_<Ctrl-D>_/~
+*
+*
+*  name: Whelpley
+*  date: 01 Feb 2019
+*  project: Demo 4B
+*
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    char myString[10];
+
+    printf("Enter a string: ");
+    fgets(myString, sizeof(myString), stdin);
+    printf("Your entered string: ");
+    fputs(myString, stdout);
+
+    return 0;
+
+}
