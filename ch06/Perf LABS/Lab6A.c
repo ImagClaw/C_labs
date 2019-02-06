6b9d6a7282bb30d14f678c52f7c8fc8449fe6bb4
ImagClaw <dal.whelpley@gmail.com>
2019-02-06 10:30:42 -0600

    Added Lab6A

---
+++ ch06/Perf LABS/Lab6A.c
@@ -0,0 +1,24 @@
+/*
+*
+* name: Whelpley
+* date: 6 Feb 2019
+* project: Lab6A - Bitwise Shift Right
+*
+*    1. Input a uint32_t userInput from stdin utilizing fscanf()
+*    2. Initialize another uint32_t bitChecker to 0x01
+*    3. Use a Bitwise Shift Left on bitChecker to ensure only the left most bit is turned on
+*    4. Print the binary representation of userInput to stdout using only the following:
+*        - fprintf()
+*        - Bitwise Shift Right >>
+*        - Bitwise AND &
+*        - Relational Operator (e.g., >)
+*
+*/
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void) {
+
+
+}
