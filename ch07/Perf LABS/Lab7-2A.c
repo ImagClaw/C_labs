0bc758b5a8a9211c73dfd8ab0fddbafb84a7791f
ImagClaw <dal.whelpley@gmail.com>
2019-02-07 15:08:07 -0600

    Lab7-2A Update

---
+++ ch07/Perf LABS/Lab7-2A.c
@@ -0,0 +1,22 @@
+/*
+*
+* name: Whelpley
+* date: 7 Feb 2019
+* project: Lab7.2A - IF STATEMENT
+*/ 
+
+#include <stdio.h>
+
+int main(void) {
+    char myArray[256];
+
+    fprintf(stdout, "Enter a string: ");
+    fscanf(stdin, "%s", &myArray);
+
+    //fprintf(stdout, "%d", myArray[0]);
+
+    if (myArray[0] >= 32){
+        fprintf(stdout, "%s", myArray);
+    }
+
+}
