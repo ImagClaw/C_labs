bdf8c184f777b74415f633c57856779367c913f1
ImagClaw <dal.whelpley@gmail.com>
2019-02-21 08:56:44 -0600

    Lab10-7A update

--- ch10/Perf LABS/Lab10-7A.c
+++ ch10/Perf LABS/Lab10-7A.c
@@ -14,5 +14,36 @@
 * 
 */
 
+#include <stdio.h>
 #undef EOF
 #define EOF 66
+#define _INC_STDIO 55
+#define FOPEN_MAX 200
+#define FILENAME_MAX 101
+
+int main()
+{
+    #ifdef _INC_STDIO
+        printf("The value of _INC_STDIO is %d \n", _INC_STDIO);
+    #else
+        printf("_INC_STDIO is defined\n");
+    #endif
+    #if (EOF == 66)
+        printf("EOF is set to %d\n", EOF);
+    #else 
+        printf("EOF is set to default.\n");
+    #endif
+    #if (FOPEN_MAX < 199)
+        printf("FOPEN_MAX is within the correct range\n");
+    #else
+        printf("FOPEN_MAX is not within the correct range\n");
+    #endif
+    #if (FILENAME_MAX >= 102)
+        printf("FILENAME_MAX is too large, please change value and try again\n");
+    #elif (FILENAME_MAX <= 0)
+        prinf("FILENAME_MAX needs to be greater than 0\n");
+    #else 
+        printf("FILENAME_MAX is within the correct range\n");
+    #endif
+    return(0);
+}
